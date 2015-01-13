faa1cdcc06abbe28f1b750204528ecf84313eb83
keyur.baldha <keyur.baldha@zeuslearning.com>
2015-01-13 14:58:37 +0530

    db connection

---
+++ db.js
@@ -0,0 +1,27 @@
+var http = require('http'),
+   mysql = require("mysql");
+
+var connection = mysql.createConnection({
+   user: "root",
+   password: "",
+  database: "test"
+});
+
+http.createServer(function (request, response) {
+
+  //request.on('end', function () {
+
+      connection.query('SELECT * FROM table1;', function (error, rows, fields) {
+
+        response.writeHead(200, {
+            "Content-Type": "text/plain",
+            'Access-Control-Allow-Origin' : '*'
+        });
+        response.write(JSON.stringify(rows));
+		//response.write('keyur');
+        response.end();
+
+      });
+   //});
+
+}).listen(8080);

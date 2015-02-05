9ca9f1d192ac2efa3ddb1062262b0354a62e7f47
keyur.baldha <keyur.baldha@zeuslearning.com>
2015-02-05 18:52:14 +0530

    updates messages

--- express/login/public/javascripts/controller.js
+++ express/login/public/javascripts/controller.js
@@ -1,12 +1,41 @@
 ﻿app.controller("friendListCtrl", function ($scope,$http) {
     var site = ChatApplication.SERVER_ADDRESS;
     var page = "/getUsers";
+    $scope.currentFriendName = '';
+    $scope.currentFriendId = '';
+    $scope.msgInputBoxValue = '';
+   
+    $scope.currentFriendObj;
     $http.get(site + page)
     .success(function (response) {
         $scope.friends = response;
+        var i = 0,
+            friends = $scope.friends,
+            noOfFriends = friends.length;
+        for (; i < noOfFriends; i++) {
+            friends[i].messages = [];
+        }
     });
     $scope.getUserId = function () {
         console.log();
-        getChatWindowHTML(this.x.user_fname, this.x.user_id)
+        var friendObj;        
+        friendObj = getFriendObject(this.x.user_id);        
+        $scope.currentFriendObj = friendObj;
+      
     };
+
+    $scope.sendMessage = function () {
+        $scope.currentFriendObj.messages.push({ "1": $scope.msgInputBoxValue });
+        $scope.msgInputBoxValue = '';
+    };
+    function getFriendObject(id) {
+        var i = 0,
+            friends = $scope.friends,
+            noOfFriends = friends.length;
+        for (; i < noOfFriends; i++) {
+            if (friends[i].user_id == id) {
+                return friends[i];
+            }
+        }
+    }
 });
